8bfa40c7d92dc02c0e16a41d571800b89ec955f8
Rohit Kumar <rohitkumar892019@gmail.com>
2025-07-27 15:37:45 +0530

    BS InfiniteArraySearch Java code added

---
+++ Java_Code/BinarySearch/FirstANDLastIdx.java
@@ -0,0 +1,65 @@
+package BinarySearch;
+
+import java.util.ArrayList;
+
+public class FirstANDLastIdx {
+    public static void main(String[] args) {
+        int[] arr = { 5, 7, 7, 8, 8, 10 };
+        firstAndLastIndex(arr, 6);
+    }
+
+    static void firstAndLastIndex(int[] arr, int target) {
+        int start = 0, end = arr.length - 1;
+        int firstIdx = -1, lastIdx=-1;
+        // ArrayList<Integer> indexs = new ArrayList<>();
+        if(arr.length==0)
+           
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                if (mid == 0 || arr[mid - 1] != target) {
+                    firstIdx = mid;
+                    break;
+                } else {
+                    end = mid - 1;
+                }
+            }
+            else{
+
+                
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+
+        start = 0;
+        end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                if (mid == arr.length - 1 || arr[mid + 1] != target) {
+                    lastIdx = mid;
+                    break;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            else{
+
+                
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Java_Code/BinarySearch/InfiniteArraySearch.java
@@ -0,0 +1,44 @@
+package BinarySearch;
+
+public class InfiniteArraySearch {
+    public static void main(String[] args) {
+        
+        int[] arr = { 2, 3, 4, 5, 7, 9, 11, 33, 34, 36, 48, 56, 98, 101 };
+        System.out.println(infiniteArraySearch(arr, 48));
+        
+    }
+    
+    static int infiniteArraySearch(int[] arr, int target) {
+        int start=0;
+        int end=1;
+
+        while(target>arr[end]){
+            int temp=end+1;
+
+            end= end+ (temp-start)*2;
+            start=temp;
+        }
+        return SmallestGreater(arr, target, start, end);
+        
+    }
+    
+    static int SmallestGreater(int[] nums, int target, int startIndex, int endIndex) {
+        int st = startIndex, end = endIndex;
+        
+
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            
+            if (nums[mid] < target) {
+                st = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+        
+    }
+}

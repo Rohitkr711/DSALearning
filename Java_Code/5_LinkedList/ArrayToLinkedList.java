638a8976180c4039da2f5e252ee2daa4dc0c91a6
Rohit Kumar <rohitkumar892019@gmail.com>
2025-10-03 11:50:15 +0530

    head and tail deletion of LL code added

--- Java_Code/5_LinkedList/ArrayToLinkedList.java
+++ Java_Code/5_LinkedList/ArrayToLinkedList.java
@@ -25,20 +25,56 @@ public class ArrayToLinkedList {
 
     }
 
-    public static void main(String[] args) {
-
-        int[] arr = { 2, 3, 4, 5, 9 };
-        // Linked list creation
-        Node head = convertArrayToLinkedList(arr);
-        System.out.println("Output:"+head.data);
-        
-        // Linked List Traversal
-        System.out.println("LinkedList traversal");
-        Node temp = head;
+    static void printLL(Node head){
+    //    System.out.println("Linked List elements");
+       Node temp = head;
         while(temp!=null){
             System.out.println(temp.data);
             temp = temp.nextNode;
         }
+    }
+
+    static Node deleteHead(Node head) {
+        if(head==null) return null;
+        Node temp = head;
+        head = head.nextNode;
+        return head;
+
+    }
+
+    static Node deleteTail(Node head){
+        if(head==null || head.nextNode==null)
+            return null;
+
+        Node temp = head;
+        while(temp.nextNode.nextNode!=null){
+            temp = temp.nextNode;
+        }
+        temp.nextNode=null;
+        return head;
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { 2, 3, 4, 5, 9 };
+        // Linked list creation
+        System.out.println("Linked List created");
+        Node head = convertArrayToLinkedList(arr);
+
+        // Linked List Traversal
+        System.out.println("Linked List traversal");
+        printLL(head);
+
+        // Delete head of LL
+        System.out.println("Linked List head deletion");
+        Node newHead = deleteHead(head);
+        printLL(newHead);
+
+        // Delete Tail of a LL
+        System.out.println("Linked List Tail deletion");
+        Node newHead1 = deleteTail(newHead);
+        printLL(newHead1);
 
     }
 }
